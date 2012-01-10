312cf3bfd777c2497b500cd352b7c9c6585a5333
Cox <jcox201@DIVWL-1318AGXA.cable.comcast.com>
2012-01-10 13:01:19 -0500

    Fixed the test

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -2,7 +2,7 @@ describe "String" do
 	context "when calling clear" do
 		it 'should clear the string, rendering it empty' do
 			stripMe = 'Remember...there\'s always money in the banana stand.'
-			stripMe.clear.should == '[cleared string]'
+			stripMe.clear.should == ''
 		end
 	end
 end
